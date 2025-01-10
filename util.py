e95b1a999f3f1fe2ba82e6711a0457aee7c52105
J0hNnY1ee <joh1eenny@gmail.com>
2025-01-10 17:26:45 +0800

    🎉 基本上实现了爬取所有音乐

--- util.py
+++ util.py
@@ -1,19 +1,35 @@
-'''
+"""
 Author: J0hNnY1ee joh1eenny@gmail.com
 Date: 2025-01-07 15:25:26
 LastEditors: J0hNnY1ee joh1eenny@gmail.com
-LastEditTime: 2025-01-09 17:00:24
+LastEditTime: 2025-01-10 15:30:48
 FilePath: /BiscuitDownloader/util.py
 Description: 
 
 Copyright (c) 2025 by J0hNnY1ee joh1eenny@gmail.com, All Rights Reserved. 
-'''
-import requests , re , json
+"""
+
+import requests, re, json, os, html
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs
 
 
-def get_html(url):
+def decode_html_entities(s):
+    # 如果字符串为空或者不是字符串类型，则直接返回
+    if not isinstance(s, str) or not s:
+        return s
+
+    # 重复解码直到没有变化
+    prev_s = None
+    while s != prev_s:
+        prev_s = s
+        s = html.unescape(s)
+    return s
+
+
+def get_html(url, cookie=""):
     try:
-        response = requests.get(url)
+        response = requests.get(url, cookies={"cookie": cookie})
         response.raise_for_status()
         response.encoding = response.apparent_encoding
 
@@ -21,30 +37,29 @@ def get_html(url):
     except requests.RequestException as e:
         print(e)
         return False
-    
-    
-    
-    
-def download(filename,url):
+
+
+def download(filename, url):
+    filename = decode_html_entities(filename)
     try:
         response = requests.get(url, stream=True)
         response.raise_for_status()
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, "wb") as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
         print(f"文件已成功下载并保存到 {filename}")
     except requests.exceptions.RequestException as e:
         print(f"文件下载失败: {e}")
-    
-    
+
 
 def extract_data_from_html(content):
     # 使用正则表达式查找window.__DATA__中的内容
-    match = match = re.search(r'window.__DATA__\s*=\s*({.*?});', content, re.DOTALL)
+    match = match = re.search(r"window.__DATA__\s*=\s*({.*?});", content, re.DOTALL)
     if not match:
         print("未能找到window.__DATA__对象")
         return None
-    
+
     data_str = match.group(1)
     try:
         # 将字符串解析为JSON对象
@@ -52,26 +67,113 @@ def extract_data_from_html(content):
     except json.JSONDecodeError as e:
         print(f"JSON解析错误: {e}")
         return None
-    
+
     try:
-        nick_value = data.get('detail', {}).get('nick', 'Nick not found')
-        song_name = data.get('detail', {}).get('song_name', 'Song name not found')
-        play_url = data.get('detail', {}).get('playurl', 'Play url not found')
+        nick_value = data.get("detail", {}).get("nick", "Nick not found")
+        song_name = data.get("detail", {}).get("song_name", "Song name not found")
+        play_url = data.get("detail", {}).get("playurl", "Play url not found")
+
         print(f"The nick value in detail is: {nick_value}")
         print(f"The song name in detail is: {song_name}")
         print(f"The play url in detail is: {play_url}")
     except AttributeError:
         print("The structure of the data does not match or data is None.")
     return nick_value, song_name, play_url
-    
-    
-    
-    
-if __name__ == '__main__':
-    url = 'https://static-play.kg.qq.com/node/LjdYJwfYwz/play_v2?s=y4W6QvyXzvUtYyn-&shareuid=6a9a9c84272e348d&abtype=13&shareDescABType=1&topsource=a0_pn201001006_z11_u1160167006_l0_t1736182334__&pageId=homepage_guest'
-    html_content = get_html(url)
-    if html_content:
-        extract_data_from_html(html_content)
-    else:
-        print("未能成功获取HTML内容")
-    
+
+
+def getSongs(uid, cookie=""):
+    header = {
+        # 定义手机的UA，因为触屏版本才可以获取所有音乐
+        # 空的Referer不能使用
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+        "Referer": "https://static-play.kg.qq.com/",
+    }
+    totalCount = 0  # 可以获取到的歌曲总数
+    songList = []  # 全部歌曲列表
+    user_information = {}  # 用户基本信息
+    res = requests.get(
+        f"https://kg.qq.com/node/personal?uid={uid}", cookies={"cookie": cookie}
+    )
+
+    if res.ok:
+        for script in BeautifulSoup(res.text, "lxml").find_all("script"):
+            if "window.__DATA__" in script.text:
+                user_information = json.loads(
+                    script.text[script.text.find("{") : script.text.rfind("};") + 1]
+                )["data"]
+                totalCount = user_information["ugc_total_count"]
+                print(f"当前用户总歌曲数：{totalCount}")
+
+                num = 15
+                n = 1
+                while n:
+                    url = f"https://node.kg.qq.com/fcgi-bin/kg_ugc_get_homepage?"
+                    params = {
+                        "outCharset": "utf-8",
+                        "from": "1",
+                        "nocache": "",
+                        "format": "json",
+                        "type": "get_uinfo",
+                        "start": n,
+                        "num": num,
+                        "share_uid": uid,
+                        "g_tk": "",
+                        "g_tk_openkey": "",
+                    }
+                    res = requests.get(url, params=params, headers=header)
+                    if res.ok:
+                        try:
+                            start_brace_index = res.text.find("{")
+                            end_brace_index = res.text.rfind("}")
+                            if start_brace_index != -1 and end_brace_index != -1:
+                                song_information = json.loads(
+                                    res.text[start_brace_index : end_brace_index + 1]
+                                )["data"]
+                                if not song_information["ugclist"]:
+                                    break
+                                songList += song_information["ugclist"]
+                                n += 1
+                            else:
+                                print("响应文本中找不到有效的 JSON 格式")
+                        except json.JSONDecodeError as e:
+                            print(f"JSON 解析错误: {e}")
+                break
+        else:
+            print("未发现歌曲！")
+        # open(
+        #     f'{user_information["kgnick"]}/{user_information["kgnick"]}_{uid}.json',
+        #     "w",
+        #     encoding="utf-8",
+        # ).write(json.dumps(songList, indent=4, ensure_ascii=False))
+    return songList
+
+
+def downloadSongs(songList, cookie=""):
+    for _, song in enumerate(songList):
+        content = get_html(
+            f"https://node.kg.qq.com/play?s={song['shareid']}", cookie=cookie
+        )
+        nick_name, song_name, play_url = "", "", ""
+        if content:
+            nick_name, song_name, play_url = extract_data_from_html(content)
+        else:
+            print("未能成功获取HTML内容")
+        download(f"downloads/{nick_name}/{nick_name}-{song_name}.m4a", play_url)
+
+
+def getuid(url):
+    parsed_url = urlparse(url)
+
+    # 提取查询参数
+    query_params = parse_qs(parsed_url.query)
+
+    # 获取uid参数值
+    uid = query_params.get("uid", [None])[0]
+
+    return uid
+
+
+
+if __name__ == "__main__":
+    url = "https://static-play.kg.qq.com/node/personal_v2?uid=6a9a9c84272e348d&shareUid=6799998d2c2b368f&pageId=homepage_guest"
+    print(getuid(url))
